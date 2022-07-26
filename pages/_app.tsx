b3ef23405f33454c68076a3fa1adb0500378910e
Kevin Lohman <badpirate@gmail.com>
2022-07-26 16:13:43 -0700

    SSR Wrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,30 +7,36 @@ import { SessionProvider } from 'next-auth/react'
 import { DefaultSeo } from 'next-seo'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { SSRProvider } from 'react-bootstrap'
 
 config.autoAddCss = false
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-  <SessionProvider session={session}>
-    <DefaultSeo
-      openGraph={{
-        type: 'website',
-        locale: 'en_US',
-        site_name: 'DrunkCircle',
-        url: `${process.env.NEXT_PUBLIC_URL}${useRouter().asPath}`,
-        images: [{
-          url: `${process.env.NEXT_PUBLIC_URL}/og_dc.png`,
-          width: 1200,
-          height: 630,
-          alt: 'DrunkCircle Banner',
-          type: 'image/png',
-        }],
-      }}
-      key="seo"
-    />
-    <Component {...pageProps} key="component" />
-  </SessionProvider>
-)
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  const CPT : any = Component
+  return (
+    <SSRProvider>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'en_US',
+            site_name: 'DrunkCircle',
+            url: `${process.env.NEXT_PUBLIC_URL}${useRouter().asPath}`,
+            images: [{
+              url: `${process.env.NEXT_PUBLIC_URL}/og_dc.png`,
+              width: 1200,
+              height: 630,
+              alt: 'DrunkCircle Banner',
+              type: 'image/png',
+            }],
+          }}
+          key="seo"
+        />
+        <CPT {...pageProps} key="component" />
+      </SessionProvider>
+    </SSRProvider>
+  )
+}
 
 export default App
